9eb3f922206bf11540c4c92503838321a6e3b9e8
crccheck <c@crccheck.com>
2017-10-12 15:41:24 -0500

    Grab a url from GET and pull it locally
    Squashed commit of the following:
    
    commit 984f7732086ec0957ce5dd8c5e0448f911ea70ae
    Author: crccheck <c@crccheck.com>
    Date:   Thu Oct 12 15:41:14 2017 -0500
    
        Delete unused req
    
    commit ef61b7e5c205dfb212b6541fc442ece693390c4a
    Author: crccheck <c@crccheck.com>
    Date:   Thu Oct 12 15:40:43 2017 -0500
    
        error handling
    
    commit 4edff4f73520e5f8e0d9460fca8cb5937a834a0b
    Author: crccheck <c@crccheck.com>
    Date:   Thu Oct 12 15:37:12 2017 -0500
    
        get a feed

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
 
 from aiohttp import web
+import aiohttp
 
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+async def refeed(request):
+    try:
+        feed_url = request.GET['feed']
+    except KeyError:
+        return web.Response(status=400, text='Must supply a ?feed=<rss url>')
+
+    try:
+        async with aiohttp.ClientSession() as session:  # headers=
+            resp = await session.get(feed_url)
+            text = await resp.text()
+        return web.Response(text=text)
+    except ValueError as e:
+        return web.Response(status=400, text=str(e))
+
 
 app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_get('/{name}', handle)
+app.router.add_get('/refeed/', refeed)
 
 port = os.getenv('PORT', 8080)
 web.run_app(app, port=port)
