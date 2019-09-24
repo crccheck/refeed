53bd5e8ee2cbffa7f29181b09d90fbb87ad673d7
crccheck <c@crccheck.com>
2019-09-24 14:51:19 -0500

    add failing test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,15 +16,32 @@ def tree():
 
 
 @pytest.fixture
-def html_tree():
+def html_article_tree():
     path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "fixtures", "article.html"
     )
     return html_parse(path)
 
 
-def test_build_item(html_tree):
-    item = build_item_context(html_tree)
+@pytest.fixture
+def html_gallery_tree():
+    path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "fixtures", "gallery.html"
+    )
+    return html_parse(path)
+
+
+def test_build_article(html_article_tree):
+    item = build_item_context(html_article_tree)
+    assert (
+        "https://media.architecturaldigest.com/photos/59dfb79cc9ed4c222543c068/master/pass/high-gloss-paint-1.jpg"
+        in item["description"]
+    )
+    assert "" in item["description"]
+
+
+def test_build_gallery(html_gallery_tree):
+    item = build_item_context(html_gallery_tree)
     assert (
         "https://media.architecturaldigest.com/photos/59dfb79cc9ed4c222543c068/master/pass/high-gloss-paint-1.jpg"
         in item["description"]
